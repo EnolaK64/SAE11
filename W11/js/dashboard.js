d7897431d569867a519b12d0efa9ba5858a40fa5
EnolaK <enolak64@gmail.com>
2026-01-13 14:52:43 +0100

    K | commentaire

--- W11/js/dashboard.js
+++ W11/js/dashboard.js
@@ -1,73 +1,91 @@
+// on recupère les element dont nous avons besoin
 const liaisonSelect = document.getElementById("liaison");
 const dateSelect = document.getElementById("date");
 const tableLiaison = document.getElementById("tableauLiaison");
 const affichageErreur = document.getElementById("erreur");
 
-chercherLesLiaisons();
+chercherLesLiaisons(); // on appele la fonction chercherLesLiaisons
 
+//on applique des écouteurs sur les different champs d'entrée pour appeler la fonction actualiserTableau quand une valeur est modifier
 liaisonSelect.addEventListener("change", actualiserTableau);
 dateSelect.addEventListener("change", actualiserTableau);
 
 function actualiserTableau() {
+	// la fonction affiche les informations dans le tableau html
 	const liaisonValue = liaisonSelect.value;
 	const dateValue = dateSelect.value;
+	// on verifie que les entrée en sont pas vides
 	if (liaisonValue !== "" && dateValue !== "") {
+		//si les entrées sont correcte on cherche la liaison choisi par l'utilisateur
 		chercherUneLiaison(liaisonValue, dateValue);
 	}
 }
 
 function chercherLesLiaisons() {
+	//la fonction envoie une requete à l'api pour récupere toutes les liaisons
 	fetch("https://can.iutrs.unistra.fr/api/liaison/all")
 		.then((response) => {
 			response.json().then((data) => {
-				creerLiaison(data);
+				creerLiaison(data); // on appele la fonction suivante pour afficher
 			});
 		})
 		.catch((e) => {
+			//.catch est appelé en cas d'exception à l'envoie de la rêquete
 			erreurTrouve(e);
 		});
 }
 
 function chercherUneLiaison(idLiaison, date) {
+	//la fonction envoie une requete à l'api pour recuperé les informations la traversée saisi
 	fetch(
 		`https://can.iutrs.unistra.fr/api/liaison/${idLiaison}/remplissage/${date}`
 	).then((response) => {
 		if (response.status === 404) {
+			//si le status est 404 alors la la traversée n'existe pas
 			traverserInexistante();
-		} else if (response.status !== 200) {
-			erreurTrouve(response.status);
-		} else {
+		} else if (response.status === 200) {
+			//si le status est 200 alors tout est ok
 			response.json().then((data) => {
 				console.log(data);
-				afficherLiaison(data);
+				afficherTraversee(data);
 			});
+		} else {
+			// sinon une erreur est survenue on envoie alors le code correspondant
+			erreurTrouver("code" + response.status);
 		}
 	});
 }
 
 function creerLiaison(data) {
+	// la fonction traite tout les liaison pour en faire des option dans le menu deroulant daans le html
 	for (let i = 0; i < Object.keys(data).length; i++) {
+		//on commence par initialiser les variables key et element
 		const key = Object.keys(data)[i];
-		const element = data[key];
+		const liaison = data[key];
 
+		//on crée un element option
 		const option = document.createElement("option");
 
-		option.innerText = element.nom;
-		option.value = element.id;
+		//on lui donne un text et une valeur
+		option.innerText = liaison.nom;
+		option.value = liaison.id;
 
+		// on l'ajoute à liste des enfants de l'element liaisonSelect
 		liaisonSelect.appendChild(option);
 	}
 }
 
-const tableBody = document.getElementById("tableauLiaisonBody");
+const tableBody = document.getElementById("tableauLiaisonBody"); // on selectione le corps du tableau
 
-function afficherLiaison(data) {
-	effacerContenu(tableBody);
+function afficherTraversee(data) {
+	//la fonction remplis le tableau avec la traversée reçu en parametre
+	effacerContenu(tableBody); // on efface ce qu'il pourrait deja y avoir dans le corps
 
 	for (let i = 0; i < Object.keys(data).length; i++) {
 		const key = Object.keys(data)[i];
 		const value = data[key];
 
+		//on calcule le taux de remplissage pour les passagers et les Voitures
 		const tauxPassagers = Math.round(
 			(value.nbReservationPassagers / value.capacitePassagers) * 100
 		);
@@ -75,14 +93,19 @@ function afficherLiaison(data) {
 			(value.nbReservationVoitures / value.capaciteVoitures) * 100
 		);
 
+		//on crée tout les element à mettre dans le tableau
 		const ligneTable = document.createElement("tr");
+
 		const caseHeure = document.createElement("td");
 		const casePassager = document.createElement("td");
 		const caseVehicule = document.createElement("td");
 
+		//on remplis le text des case
 		caseHeure.innerText = value.heure;
 		casePassager.innerText = tauxPassagers + "%";
 		caseVehicule.innerText = tauxVoitures + "%";
+
+		//on applique une classe qui donnera la couleur à la case selon taux de remplissage
 		if (tauxPassagers < 50) {
 			casePassager.classList.add("vert");
 		} else if (tauxPassagers >= 50 && tauxPassagers < 75) {
@@ -92,7 +115,7 @@ function afficherLiaison(data) {
 		} else {
 			casePassager.classList.add("fluo");
 		}
-
+		// de même pour les voitures
 		if (tauxVoitures < 50) {
 			caseVehicule.classList.add("vert");
 		} else if (tauxVoitures >= 50 && tauxVoitures < 75) {
@@ -103,14 +126,20 @@ function afficherLiaison(data) {
 			caseVehicule.classList.add("fluo");
 		}
 
+		//on ajoute les case à la liste des enfants de la ligne
 		ligneTable.append(caseHeure, casePassager, caseVehicule);
+		// et on ajoute la ligne au tableau
 		tableBody.appendChild(ligneTable);
 
+		//si on est arrivé jusque là c'est que tout c'es bien passer
+		//on peut donc supprimer tout message d'erreur qu'il pourrait y avoir
 		affichageErreur.innerText = "";
+		//et afficher le tableau
 		tableLiaison.classList.add("afficherTable");
 	}
 }
 
+// les deux fonction suivante affiche des messages d'erreur
 function traverserInexistante() {
 	affichageErreur.innerText = "Aucun traversée n'a était trouvé";
 	tableLiaison.classList.remove("afficherTable");
@@ -121,6 +150,7 @@ function erreurTrouve(message) {
 }
 
 function effacerContenu(element) {
+	//la fonction supprime tout les enfant de l'element reçu en paramètre
 	const enfants = element.children;
 	const length = enfants.length;
 	for (let i = 0; i < length; i++) {
